e05a06f6bda466bfecc89bc004af7013e81a069b
vzhev <v.zhevaga@gmail.com>
2023-02-09 10:02:07 +0100

    fixed hw 02.03

--- homeworks/02.02.2023/hw.sql
+++ homeworks/02.02.2023/hw.sql
@@ -1,37 +1,42 @@
-create database DITOK;
+create database if not exists DITOK;
 use DITOK;
-create table users(
+create table if not exists user (
 	id integer primary key auto_increment,
 	username varchar(100) not null,
-    datetime datetime,
     email varchar(100) not null,
     password varchar(100) not null,
-    is_blocked boolean
+    created_at datetime default current_timestamp,
+    is_blocked boolean default false
  );
 
- create table videos(
+ create table if not exists videos(
 	id integer primary key auto_increment,
 	title varchar(100) not null,
-    datetime datetime,
+    added_at datetime default current_timestamp,
     description varchar(1000),
-    duration_session integer,
-    user_id integer
+    duration_secs integer,
+    user_id integer,
+    foreign key (user_id) references Users(id)
  );
  
-  create table reactions(
+  create table if not exists reactions(
 	id integer primary key auto_increment,
     type char(7) check (type in("Like" , "DisLike")),
-    datetime datetime,
-    user_id integer,
-	video_id integer
- );
- 
-   create table comments(
-	id integer primary key auto_increment,
-    datetime datetime,
+	added_at datetime default current_timestamp,
     user_id integer,
 	video_id integer,
-    text varchar(2000)
+	foreign key (user_id) references Users(id),
+	foreign key (video_id) references Videos(id)
+ );
+ 
+   create table if not exists comments(
+	id integer primary key auto_increment,
+	added_at datetime default current_timestamp,
+    user_id integer,
+	video_id integer,
+    text varchar(2000),
+    foreign key (user_id) references Users(id),
+	foreign key (video_id) references Videos(id)
  );
  
 select * from users;
