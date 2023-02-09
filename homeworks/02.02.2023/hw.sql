6ba78d20712abb563925aeea4e4d8cb2305e750e
vzhev <v.zhevaga@gmail.com>
2023-02-09 10:06:24 +0100

    fix hw 02.03

--- homeworks/02.02.2023/hw.sql
+++ homeworks/02.02.2023/hw.sql
@@ -13,7 +13,7 @@ create table if not exists user (
 	id integer primary key auto_increment,
 	title varchar(100) not null,
     added_at datetime default current_timestamp,
-    description varchar(1000),
+    description text(1000),
     duration_secs integer,
     user_id integer,
     foreign key (user_id) references Users(id)
@@ -34,7 +34,7 @@ create table if not exists user (
 	added_at datetime default current_timestamp,
     user_id integer,
 	video_id integer,
-    text varchar(2000),
+    text text(2000),
     foreign key (user_id) references Users(id),
 	foreign key (video_id) references Videos(id)
  );
