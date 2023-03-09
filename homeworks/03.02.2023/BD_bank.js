5ff08732ebe97aa3085efda2cfb366d8f94448c4
vzhev <v.zhevaga@gmail.com>
2023-03-09 10:38:45 +0100

    fixed hw

--- homeworks/03.02.2023/BD_bank.js
+++ homeworks/03.02.2023/BD_bank.js
@@ -27,6 +27,12 @@ db.users.find({
 	is_blocked: true,
 	balance: 0,
 });
+// при отсутствии поля balance:
+db.users.find({
+	country: 'China',
+	is_blocked: true,
+	$or: [{ balance: 0 }, { balance: { $exists: false } }],
+});
 // (5) пользователям не из Китая и не из США, имеющим баланс более 5000, установить статус премиум
 db.users.updateMany(
 	{
@@ -41,6 +47,14 @@ db.users.updateMany(
 
 db.users.find({
 	$nor: [{ country: 'China' }, { balance: { $lt: 100 } }],
+});
+// правильно
+db.users.find({
+	$nor: [
+		{ country: 'China' },
+		{ balance: { $lt: 100 } },
+		{ balance: { $exists: false } },
+	],
 });
 
 // -> поиск пользователей не из Китая с балансом не менее 100
